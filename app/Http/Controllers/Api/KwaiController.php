9751d09a7fe4c57e46f68348cfead55e38906cb8
guider <guider@yeah.net>
2023-03-03 14:21:43 +0800

    add

--- app/Http/Controllers/Api/KwaiController.php
+++ app/Http/Controllers/Api/KwaiController.php
@@ -19,6 +19,18 @@ class KwaiController extends Controller
         $kwailog = new KwaiLog($request->all());
         $kwailog->raw_data = json_encode($request->all());
 //        print_r($kwailog->toArray());
-        $kwailog->save();
+        if ($kwailog->save()) {
+            $callback = $request->query('callback');
+            $eventid = $request->query('eventid');
+            $timestamp = time();
+            $callback_url = "https://k.kuaishou.com/rest/web/task/debug/track/activate?callback=$callback&event_type=$eventid&event_time=$timestamp";
+            return redirect($callback_url);
+
+//            return response()->json(['code' => 0, 'msg' => 'success']);
+        } else {
+            return response()->json(['code' => 1, 'msg' => 'failed']);
+
+        }
+
     }
 }
