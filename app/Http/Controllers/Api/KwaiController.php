5591fe80d758169e2b1cb363928e25d446d0ed2d
guider <guider@yeah.net>
2023-03-03 14:33:33 +0800

    add

--- app/Http/Controllers/Api/KwaiController.php
+++ app/Http/Controllers/Api/KwaiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Models\KwaiLog;
+use Illuminate\Support\Facades\Http;
 
 class KwaiController extends Controller
 {
@@ -13,18 +14,17 @@ class KwaiController extends Controller
     public function monitor(Request $request)
     {
 
-        print_r($request->query('imei'));
-        print_r($request->query());
-        print_r($request->all());
         $kwailog = new KwaiLog($request->all());
         $kwailog->raw_data = json_encode($request->all());
 //        print_r($kwailog->toArray());
         if ($kwailog->save()) {
             $callback = $request->query('callback');
-            $eventid = $request->query('eventid');
+            $event_id = $request->query('event_id');
             $timestamp = time();
-            $callback_url = "https://k.kuaishou.com/rest/web/task/debug/track/activate?callback=$callback&event_type=$eventid&event_time=$timestamp";
-            return redirect($callback_url);
+            $callback_url = "https://k.kuaishou.com/rest/web/task/debug/track/activate?callback=$callback&event_type=$event_id&event_time=$timestamp";
+            $resp = Http::get($callback_url);
+            return $resp;
+//            return redirect($callback_url);
 
 //            return response()->json(['code' => 0, 'msg' => 'success']);
         } else {
